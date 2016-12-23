11f341ade3884b11d5cdf72b9b432181d3bb5d55
Luna Versity <mrgodzbrother@gmail.com>
2016-12-22 17:49:48 -0800

    re-wrote
    Re wrote the whole thing, including better SQL processes (mysqli is shit)

--- web/index.php
+++ web/index.php
@@ -1,38 +1,57 @@
-<!DOCTYPE HTML>
-<html>
+<?php
+// Edit this --->
+$db_host = "";
+$db_name = "";
+$db_user = "";
+$db_pass = "";
+// Edit this <---
+try{
+$host = "mysql:host=$db_host;dbname=$db_name";
+$pdo = new PDO($host, $db_user, $db_pass);
+}
+catch(PDOException $pdo_err){ ?>
+    <h1 style="color:#c10000;">Error Caught:</h1>
+    <h2><?= $pdo_err->getMessage() ?></h2>
+  <?php die();
+}
+// Use PDO instead of MySQLi please.
+$bug_report = $pdo->prepare("SELECT * FROM `bug_report`"); // Prepare all statements
+$bug_report->execute(); // Execute it
+$bug = $bug_report->fetchAll(PDO::FETCH_ASSOC); // Grab the data
+
+?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Bug Reports</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Bug Reports</title>
 </head>
-
 <body>
-<br>
-<br>
-<br>
-<br>
-<center><table border="2">
+  <div class="center-page">
+    <div>
+      <tbody>
+      <table>
+        <tr><td colspan="2">Bug Reports</td></tr>
         <tr>
-            <td colspan="2">Bug Reports and Suggestions</td>
+          <td>SteamID</td>
+          <td>Description</td>
         </tr>
-        <tr>
-            <td><b><i>SteamID</i></b></td>
-            <td><b><i>Bug Description</i></b></td>
-        </tr>
-        <?php
-        $connection = new mysqli("localhost", "user", "password", "db_table");
-
-        $result = mysqli_query($connection, "SELECT SteamID,BugReport FROM bug_report");
-
-        while($row = mysqli_fetch_assoc($result)){   //Creates a loop to loop through results
-            echo "<tr>" . "<td>" . $row['SteamID'] . "</td>" . "<td>" . $row['BugReport'] . "</td>" .  "</tr>" . "<br>"; 
-        }
-
-        mysqli_close($connection); //Make sure to close out the database connection
-        ?>
-    </table></center>
-</body>
-
-<footer>
-    <center><p>&copy;2016 Subject_Alpha</p></center>
-</footer>
+  <?php foreach($bug as $bu=>$b): ?>
+    <tr>
+      <td>
+        <?php echo $b['SteamID']; ?>
+      </td>
+      <td>
+        <?php echo $b['BugReport']; ?>
+      </td>
+    </tr>
+  <?php endforeach; ?>
+          </tbody>
+        </table>
+      </div>
+    </div>
+<span class="credits">&copy; 2016 Subject_Alpha</span>
+  </body>
 </html>
