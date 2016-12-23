0054fd549c336e306b45ff635413e9a38719d4a7
Luna Versity <mrgodzbrother@gmail.com>
2016-12-23 00:14:23 -0800

    link styles
    forgot

--- web/index.php
+++ web/index.php
@@ -27,6 +27,7 @@ $bug = $bug_report->fetchAll(PDO::FETCH_ASSOC); // Grab the data
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Bug Reports</title>
+  <link href="style.css" rel="stylesheet">
 </head>
 <body>
   <div class="center-page">
